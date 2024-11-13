70d3a2d87d96f01cb7ef68ca75dceb7031ae8ae5
Abdul Hakim Akash <abdulhakimakash209@gmail.com>
2024-11-13 22:50:58 +0600

    Add files via upload

---
+++ week3/day4/E - EVacuate to Moon.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#include <vector>
+int i, j, k;
+#define ll long long
+#define f1 for (int i = 0; i < n; i++)
+#define f2 for (int i = 1; i < N; i++)
+#define fr for (int i = n; i > n; i--)
+#define fre for (int i = n; i >= n; i--)
+#define f3 for (int j = 0; j < n; j++)
+#define f4 for (int j = n; j > n; j--)
+#define f5 for (int i = n - 1; i >= 0; i--)
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define Yes cout << "Yes" << endl;
+#define No cout << "No" << endl;
+#define vi vector<long long>
+#define v_input(v, n)             \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cin>>v[i];             \
+                       \
+                       \
+    };
+#define v_output(v, n)            \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cout << v[i] << " ";    \
+    }                           \
+    cout << endl;
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+   
+   int t;
+   cin>>t;
+   while (t--)
+   {
+    /* code */
+    int n,m,h;
+    cin>>n>>m>>h;
+    vi car(n);
+    vi pwer(m);
+    v_input(car,n);
+    v_input(pwer,m);
+    sort(car.begin(),car.end(), greater<int>());
+    sort(pwer.begin(),pwer.end(), greater<int>());
+    ll l =0,r=0;
+    ll ans = 0;
+    while (l<n && r<m)
+    {
+        ans = ans+min(pwer[r]*h,car[l]);
+        l++;
+        r++;
+    }
+    cout<<ans<<endl;
+    
+   }
+
+    return 0;
+}
