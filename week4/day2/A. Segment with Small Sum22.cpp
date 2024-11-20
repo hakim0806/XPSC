75017f8c22f291353297ceff49c3342b70292df9
Abdul Hakim Akash <abdulhakimakash209@gmail.com>
2024-11-20 18:28:09 +0600

    Add files via upload

---
+++ week4/day2/A. Segment with Small Sum22.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#include <vector>
+int i, j, k;
+#define ll long long
+#define f1 for (int i = 0; i < n; i++)
+#define f2 for (int i = 1; i < N; i++)
+#define fr for (int i = n; i > n; i--)
+#define fre for (int i = n; i >= n; i--)
+#define f3 for (int j = 0; j < n; j++)
+#define f4 for (int j = n; j > n; j--)
+#define f5 for (int i = n - 1; i >= 0; i--)
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define Yes cout << "Yes" << endl;
+#define No cout << "No" << endl;
+#define vi vector<long long>
+#define v_input(v, n)           \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cin >> v[i];            \
+    };
+#define v_output(v, n)          \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cout << v[i] << " ";    \
+    }                           \
+    cout << endl;
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    ll k;
+    cin >> n >> k;
+    vi v(n);
+    v_input(v, n);
+
+    int l = 0, r = 0;
+    ll ans = 0;
+    ll sum = 0;
+    while (r < n)
+    {
+        sum += v[r];
+        if (sum <= k)
+        {
+            ans = max(ans,(ll) (r - l + 1));
+        }
+        else
+        {
+            sum -= v[l];
+            l++;
+        }
+        r++;
+    }
+
+    cout << ans << endl;
+    return 0;
+}
