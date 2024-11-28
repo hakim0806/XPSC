4220d491ff7db7fd7184ba28c61d0a571427815d
Abdul Hakim Akash <abdulhakimakash209@gmail.com>
2024-11-28 22:43:55 +0600

    Add files via upload

---
+++ week5/day5/B. Mex Master.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+#include <vector>
+int i, j, k;
+#define ll long long
+#define f1 for (int i = 0; i < n; i++)
+#define f2 for (int i = 1; i < N; i++)
+#define fr for (int i = n; i > n; i--)
+#define fre for (int i = n; i >= n; i--)
+#define f3 for (int j = 0; j < n; j++)
+#define f4 for (int j = n; j > n; j--)
+#define f5 for (int i = n - 1; i >= 0; i--)
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define Yes cout << "Yes" << endl;
+#define No cout << "No" << endl;
+#define vi vector<long long>
+#define v_input(v, n)             \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cin >> v[i];            \
+    };
+#define v_output(v, n)            \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cout << v[i] << " ";    \
+    }                           \
+    cout << endl;
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vi a(n);
+        v_input(a, n);
+        ll zero = 0;
+        ll others = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] != 0)
+                others++;
+            else
+                zero++;
+        }
+        ll ans;
+        if (zero == 0)
+        {
+            ans = 0;
+        }
+        else
+        {
+            if (others >= zero - 1)
+            {
+                ans = 0;
+            }
+            else
+            {
+                sort(a.begin(),a.end(),greater<int>());
+                int mx = a[0];                
+                if (mx == 1)
+                {
+                    ans = 2;
+                }
+                else
+                {
+                    ans = 1;
+                }
+            }
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
