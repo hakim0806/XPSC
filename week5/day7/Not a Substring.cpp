559ca183e3af7da02b5845f9998b87faf7f42da5
Abdul Hakim Akash <abdulhakimakash209@gmail.com>
2024-11-30 23:31:57 +0600

    Add files via upload

---
+++ week5/day7/Not a Substring.cpp
@@ -0,0 +1,72 @@
+//by hint
+#include <bits/stdc++.h>
+#include <vector>
+int i, j, k;
+#define ll long long
+#define f1 for (int i = 0; i < n; i++)
+#define f2 for (int i = 1; i < N; i++)
+#define fr for (int i = n; i > n; i--)
+#define fre for (int i = n; i >= n; i--)
+#define f3 for (int j = 0; j < n; j++)
+#define f4 for (int j = n; j > n; j--)
+#define f5 for (int i = n - 1; i >= 0; i--)
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define Yes cout << "Yes" << endl;
+#define No cout << "No" << endl;
+#define vi vector<long long>
+#define v_input(v, n)             \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cin>>v[i];             \
+                       \
+                       \
+    };
+#define v_output(v, n)            \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cout << v[i] << " ";    \
+    }                           \
+    cout << endl;
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        int n = s.size();
+        string t1, t2;
+        for (int i = 0; i < n; i++)
+        {
+            t1 += "()";
+        }
+        for (int i = 0; i < n; i++)
+        {
+            t2 += '(';
+        }
+        for (int i = 0; i < n; i++)
+        {
+            t2 += ')';
+        }
+        if (t1.find(s) >= t1.size())
+        {
+            YES;
+            cout << t1 << endl;
+        }
+        else if (t2.find(s) >= t2.size())
+        {
+            YES;
+            cout << t2 << endl;
+        }
+        else
+        {
+            NO;
+        }
+    }
+    return 0;
+}
