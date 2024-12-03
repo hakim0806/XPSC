a8504fd6da9a3e4ea07c57b582d96f382c1b2d0c
Abdul Hakim Akash <abdulhakimakash209@gmail.com>
2024-12-03 23:32:24 +0600

    Add files via upload

---
+++ week6/day3/C - Little Nikita.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define all(a) a.begin(), a.end()
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define rev(n) n.rbegin(), n.rend()
+#define pb push_back
+#define Y cout << "YES" << endl
+#define N cout << "NO" << endl
+#define print(n) cout << n << endl
+#define Pi M_PI
+#define newline cout << endl
+#define vecIn(v, n)             \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        ll x;                   \
+        cin >> x;               \
+        v.pb(x);                \
+    };
+#define vecOut(v, n)            \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cout << v[i] << " ";    \
+    }                           \
+    cout << endl;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+       int n,m;
+       cin>>n>>m;
+       if(n==m)
+       {
+        Y;
+       }
+    else if(n>m)
+       {
+        int d = n-m;
+        if(d%2==0)
+        {
+            Y;
+        }
+        else 
+        {
+            N;
+        }
+       }
+       else 
+       {
+        N;
+       }
+    }
+
+    return 0;
+}
