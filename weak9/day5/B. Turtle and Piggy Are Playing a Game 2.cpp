06295a0f9d36bc60b43a236725208ee8bc35d49e
Abdul Hakim Akash <abdulhakimakash209@gmail.com>
2024-12-27 00:19:55 +0600

    Add files via upload

---
+++ weak9/day5/B. Turtle and Piggy Are Playing a Game 2.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#include <vector>
+int i, j, k;
+#define ll long long
+#define f1 for (int i = 0; i < n; i++)
+#define f2 for (int i = 1; i < N; i++)
+#define fr for (int i = n; i > n; i--)
+#define fre for (int i = n; i >= n; i--)
+#define f3 for (int j = 0; j < n; j++)
+#define f4 for (int j = n; j > n; j--)
+#define f5 for (int i = n - 1; i >= 0; i--)
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define Yes cout << "Yes" << endl;
+#define No cout << "No" << endl;
+#define vi vector<long long>
+#define v_input(v, n)             \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cin>>v[i];             \
+                       \
+                       \
+    };
+#define v_output(v, n)            \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cout << v[i] << " ";    \
+    }                           \
+    cout << endl;
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--) {
+        ll n;
+        cin >> n;
+        deque<ll> dq;
+        for (int i = 0; i < n; i++) {
+            int x;
+            cin >> x;
+            dq.push_back(x);
+        }
+        sort(dq.begin(), dq.end());
+        ll d = n / 2 + 1;
+        while (dq.size() > d) {
+            dq.pop_back();
+        }
+        cout << dq.back() << endl;
+    }
+    return 0;
+}
