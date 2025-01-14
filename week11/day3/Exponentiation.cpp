5069487c01c9bf58e87bf87c693bfb6d53d43ae5
Abdul Hakim Akash <abdulhakimakash209@gmail.com>
2025-01-14 23:28:48 +0600

    Add files via upload

---
+++ week11/day3/Exponentiation.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+#include <vector>
+int i, j, k;
+#define ll long long
+#define f1 for (int i = 0; i < n; i++)
+#define f2 for (int i = 1; i < N; i++)
+#define fr for (int i = n; i > n; i--)
+#define fre for (int i = n; i >= n; i--)
+#define f3 for (int j = 0; j < n; j++)
+#define f4 for (int j = n; j > n; j--)
+#define f5 for (int i = n - 1; i >= 0; i--)
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define Yes cout << "Yes" << endl;
+#define No cout << "No" << endl;
+#define vi vector<long long>
+#define v_input(v, n)             \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cin>>v[i];             \
+                       \
+                       \
+    };
+#define v_output(v, n)            \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cout << v[i] << " ";    \
+    }                           \
+    cout << endl;
+using namespace std;
+const int MOD = 1e9 + 7;
+int power(int x, int n) {
+   int ans = 1 % MOD;
+   while (n) {
+      if (n & 1) {
+         ans = (1LL * ans % MOD * x % MOD) % MOD;
+      }
+      x = 1LL * x * x % MOD;
+      n >>= 1;
+   }
+   return ans;
+}
+
+int main() {
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+   int t;
+   cin >> t;
+   while (t--) {
+      int x, n;
+      cin >> x >> n;
+      cout << power(x, n) << '\n';
+   }
+   return 0;
+}
