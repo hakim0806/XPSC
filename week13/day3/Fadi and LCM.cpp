ef84d99a1a66c2c93e75d6921431aa3fc67435f4
Abdul Hakim Akash <abdulhakimakash209@gmail.com>
2025-01-28 23:17:01 +0600

    Add files via upload

---
+++ week13/day3/Fadi and LCM.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#include <vector>
+int i, j, k;
+#define ll long long
+#define f1 for (int i = 0; i < n; i++)
+#define f2 for (int i = 1; i < N; i++)
+#define fr for (int i = n; i > n; i--)
+#define fre for (int i = n; i >= n; i--)
+#define f3 for (int j = 0; j < n; j++)
+#define f4 for (int j = n; j > n; j--)
+#define f5 for (int i = n - 1; i >= 0; i--)
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define Yes cout << "Yes" << endl;
+#define No cout << "No" << endl;
+#define vi vector<long long>
+#define v_input(v, n)             \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cin>>v[i];             \
+                       \
+                       \
+    };
+#define v_output(v, n)            \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cout << v[i] << " ";    \
+    }                           \
+    cout << endl;
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll x;
+    cin>>x;
+    ll r = 1;
+    for(ll i = 1; i*i <= x; i++)
+    {
+        if(x%i == 0 && __gcd(i,x/i) == 1)
+        {
+             r = i;
+        }
+    }
+    cout<< r << " " << x/r <<'\n';
+    return 0;
+}
