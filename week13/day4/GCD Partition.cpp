28f675acb6d4990c00c0cb230eb849327dfea48e
Abdul Hakim Akash <abdulhakimakash209@gmail.com>
2025-01-29 23:32:55 +0600

    Add files via upload

---
+++ week13/day4/GCD Partition.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#include <vector>
+int i, j, k;
+#define ll long long
+#define f1 for (int i = 0; i < n; i++)
+#define f2 for (int i = 1; i < N; i++)
+#define fr for (int i = n; i > n; i--)
+#define fre for (int i = n; i >= n; i--)
+#define f3 for (int j = 0; j < n; j++)
+#define f4 for (int j = n; j > n; j--)
+#define f5 for (int i = n - 1; i >= 0; i--)
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define Yes cout << "Yes" << endl;
+#define No cout << "No" << endl;
+#define vi vector<long long>
+#define v_input(v, n)             \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cin>>v[i];             \
+                       \
+                       \
+    };
+#define v_output(v, n)            \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cout << v[i] << " ";    \
+    }                           \
+    cout << endl;
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        ll n;
+        cin>>n;
+        vi v(n);
+        v_input(v,n);
+        vi pre(n);
+        for(int i = 0; i<n; i++)
+        {
+            if(i==0){
+            pre[i]=v[i];
+        }
+        else{
+            pre[i]=pre[i-1]+v[i];
+        }
+        }
+        ll ans = 0;
+        for(int i = 1; i<n; i++)
+        {
+            ans = max(ans, __gcd(pre[i-1], pre[n-1]- pre[i-1]));
+        }
+        cout<< ans <<endl;
+    }
+    return 0;
+}
