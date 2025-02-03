94e2b3cb1b5da59461b18a337d62c50b3e7ba332
Abdul Hakim Akash <abdulhakimakash209@gmail.com>
2025-02-03 23:27:32 +0600

    Add files via upload

---
+++ week13/day5/New Year and Hurry.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#include <vector>
+int i, j, k;
+#define ll long long
+#define f1 for (int i = 0; i < n; i++)
+#define f2 for (int i = 1; i < N; i++)
+#define fr for (int i = n; i > n; i--)
+#define fre for (int i = n; i >= n; i--)
+#define f3 for (int j = 0; j < n; j++)
+#define f4 for (int j = n; j > n; j--)
+#define f5 for (int i = n - 1; i >= 0; i--)
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define Yes cout << "Yes" << endl;
+#define No cout << "No" << endl;
+#define vi vector<long long>
+#define v_input(v, n)             \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cin>>v[i];             \
+                       \
+                       \
+    };
+#define v_output(v, n)            \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cout << v[i] << " ";    \
+    }                           \
+    cout << endl;
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll n , k;
+    cin>>n>>k;
+    vi v(n+1);
+    v[0] = 0;
+    for(int i = 1; i<=n; i++)
+    {
+       v[i] = i;
+       v[i] = v[i]*5;
+    }
+    ll cnt = 0; 
+    ll tmp = k;
+    for(int i = 1; i<=n; i++)
+    {
+        if(tmp <= 240)
+        {
+            tmp +=v[i];
+            if(tmp>240)
+            {
+                break;
+            }
+            cnt++;
+        }
+    }
+    cout<< cnt <<'\n';
+    return 0;
+}
