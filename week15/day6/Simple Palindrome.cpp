a227dd534add7688e5a14e2908c9ef2990808a5c
Abdul Hakim Akash <abdulhakimakash209@gmail.com>
2025-02-19 23:51:14 +0600

    Add files via upload

---
+++ week15/day6/Simple Palindrome.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+#include <vector>
+int i, j, k;
+#define ll long long
+#define f1 for (int i = 0; i < n; i++)
+#define f2 for (int i = 1; i < N; i++)
+#define fr for (int i = n; i > n; i--)
+#define fre for (int i = n; i >= n; i--)
+#define f3 for (int j = 0; j < n; j++)
+#define f4 for (int j = n; j > n; j--)
+#define f5 for (int i = n - 1; i >= 0; i--)
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define Yes cout << "Yes" << endl;
+#define No cout << "No" << endl;
+#define vi vector<long long>
+#define v_input(v, n)             \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cin>>v[i];             \
+                       \
+                       \
+    };
+#define v_output(v, n)            \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cout << v[i] << " ";    \
+    }                           \
+    cout << endl;
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        ll n; 
+        cin>>n;
+        string s = "aeiouaeiouaeiouaeiouaeiouaeiouaeiouaeiouaeiouaeiouaeiouaeiouaeiouaeiouaeiouaeiouaeiouaeiouaeiouaeiouaeiou";
+        vector<char>v;
+        for(int i = 0; i<n; i++)
+        {
+            v.push_back(s[i]);
+        }
+        sort(v.begin(),v.end());
+        for(auto x : v)
+        {
+            cout<< x;
+        }
+        cout << '\n';
+    }
+    return 0;
+}
